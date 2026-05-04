f576bdca9f67447f0b4a3c2f2c0fea4ffb145b5e
xhdndmm <xhdndmm@gmail.com>
2026-05-04 17:59:41 +0800

    ci：去掉编译为单文件

--- script/build.sh
+++ script/build.sh
@@ -1,24 +1,36 @@
+```bash
 #!/usr/bin/env bash
 set -euo pipefail
 
-project=$(realpath $(dirname $0)/..)
+project=$(realpath "$(dirname "$0")/..")
 
-if [ "$(uname -s)" == "Linux" ]; then
+if [ "$(uname -s)" = "Linux" ]; then
   OUT_NAME=123pan
-  EXTRA_ARGS=(--lto=yes)
+  EXTRA_ARGS=(
+    --standalone
+    --lto=yes
+  )
 else
   OUT_NAME=123pan.exe
-  EXTRA_ARGS=(--windows-disable-console --lto=yes)
+  EXTRA_ARGS=(
+    --standalone
+    --windows-disable-console
+    --msvc=latest
+    --static-libpython=no
+    --lto=yes
+  )
 fi
 
 (
-  cd $project
+  cd "$project"
 
   uv run -m nuitka src/123pan.py \
-    --onefile \
     --enable-plugin=pyqt6 \
     --assume-yes-for-downloads \
+    --python-flag=no_docstrings \
+    --remove-output \
     "${EXTRA_ARGS[@]}" \
     --output-filename="$OUT_NAME" \
     "$@"
-  )
+)
+```
